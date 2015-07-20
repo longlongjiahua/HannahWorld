46241cb56710c4ed704332352cec12769c741771
yongxu <xuyhanghang@gmail.com>
2015-07-20 08:05:04 -0600

    add time count

--- app/src/main/java/com/hannah/hannahworld/MakeNumberActivity.java
+++ app/src/main/java/com/hannah/hannahworld/MakeNumberActivity.java
@@ -1,12 +1,20 @@
 package com.hannah.hannahworld;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.util.Log;
 
@@ -18,8 +26,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MakeNumberActivity extends Activity implements View.OnClickListener {
-    private static final String TAG = "MakeNumberActivity";
-
     private TextView myCard;
     private static final String TextView_TAG = "The Android Logo";
     public LinearLayout lvNumber;
@@ -38,18 +44,28 @@ public class MakeNumberActivity extends Activity implements View.OnClickListener
     private DragDropHelp number2Formula;
     private DragDropHelp formula2NumberOrOperator;
     private DragDropHelp operator2Formula;
-    private Button btAnswer;
     private Button btNextQuestion;
+    private int selectedTime;
+    private int selectedTimePos=1;
+    private Spinner spTimeResource;
+    private TextView tvScore;
+    private TextView tvTimeCountDown;
+    private boolean unRegistered = false;
+    private static final String TAG = "MakeNumberActivity";
+    private boolean mServiceBound =false;
+    private BroadcastTimeCountService mService;
+    private boolean mBound = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_makenumberactivity);
-
-        btAnswer = (Button) findViewById(R.id.bt_answer);
+        tvScore = (TextView) findViewById(R.id.tv_your_score);
+        tvTimeCountDown = (TextView) findViewById(R.id.tv_time_countdown);
+        spTimeResource = (Spinner) findViewById(R.id.sp_timesource);
+        spTimeResource.setSelection(selectedTimePos);
+        spTimeResource.setOnItemSelectedListener(new CustomOnItemSelectedListener());
         btNextQuestion = (Button) findViewById(R.id.bt_submit);
-        btAnswer.setOnClickListener(this);
         btNextQuestion.setOnClickListener(this);
         questionNumbers = (ArrayList<String>) QuesionAndAnswerUtils.provide24GameQuestion();
         mNumberList = new ArrayList<String> (questionNumbers);
@@ -106,21 +122,17 @@ public class MakeNumberActivity extends Activity implements View.OnClickListener
 
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.bt_answer:
-                String answer = QuesionAndAnswerUtils.giveAnswer(questionNumbers);
-                AlertDialog alertDialog = new AlertDialog.Builder(this).create(); //Read Update
-                alertDialog.setTitle("Answer");
-                alertDialog.setMessage(answer);
-                alertDialog.setButton("Continue..", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        //  add functions
-                    }
-                });
-                alertDialog.show();
-                //Toast.makeText(this, answer, Toast.LENGTH_LONG ).show();
-                break;
-            case R.id.bt_submit:
-                if(btNextQuestion.getText().toString().equals("Submit")) {
+              case R.id.bt_submit:
+                  if(btNextQuestion.getText().toString().equals("Start")) {
+                      final Intent mServiceIntent = new Intent(MakeNumberActivity .this, BroadcastTimeCountService.class);
+                      mServiceIntent.putExtra(MathActivity.INTENT_EXTRA_MINUTES, selectedTime);
+                      Log.i("Time", ""+selectedTime);
+                      MakeNumberActivity.this.bindService(mServiceIntent, mConnection, Context.BIND_AUTO_CREATE);
+                      //MathActivity.this.startServce(mServiceIntent);
+                      btNextQuestion.setText("Submit");
+
+                  }
+                else if(btNextQuestion.getText().toString().equals("Submit")) {
                     if (judgeAnswer()) {
 
                     } else {
@@ -134,14 +146,19 @@ public class MakeNumberActivity extends Activity implements View.OnClickListener
                     btNextQuestion.setText("Next");
                 }
                 else {
-                    mFormulaList.clear();
-                    mNumberList.clear();
-                    for(String str: QuesionAndAnswerUtils.provide24GameQuestion())
-                            mNumberList.add(str);
-                    btNextQuestion .setText("Submit");
-                    numberAdapter.notifyDataSetChanged();
-                    formulaAdapter.notifyDataSetChanged();
-                }
+                      if (btNextQuestion.getText().toString().equals("Next")) {
+                          mFormulaList.clear();
+                          mNumberList.clear();
+                          questionNumbers = (ArrayList<String>) QuesionAndAnswerUtils.provide24GameQuestion();
+                          for (String str : questionNumbers)
+                              mNumberList.add(str);
+                          btNextQuestion.setText("Submit");
+                          numberAdapter.notifyDataSetChanged();
+                          formulaAdapter.notifyDataSetChanged();
+                      }
+                  }
+
+                  break;
         }
     }
     public boolean judgeAnswer(){
@@ -164,4 +181,62 @@ public class MakeNumberActivity extends Activity implements View.OnClickListener
         listData.add(insertPos, str);
         targetAdapter.notifyDataSetChanged();
     }
+
+    private class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+        private int[] times = {1,2,3,4};
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos,long id) {
+            // parent.getItemAtPosition(pos).toString();
+            selectedTime = times[pos];
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg0) {
+            // TODO Auto-generated method stub
+        }
+
+    }
+
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            BroadcastTimeCountService.MathBinder binder = (BroadcastTimeCountService.MathBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mService = null;
+
+            mBound = false;
+        }
+    };
+
+    void doUnbindService() {
+        if (mBound) {
+            // Detach our existing connection.
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            String time = intent.getStringExtra(BroadcastTimeCountService.TIMELEFT);
+            Log.i("BroadcastReceiver:::", time);
+            if(time.equals("00:00")){
+                //gridView.setEnabled(false);
+            }
+            //mathFragments[currentPageNo].tvTimeCountDown.setText(time);
+            tvTimeCountDown.setText(time);
+        }
+    };
+
 }
